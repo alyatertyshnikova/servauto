1e3769a94ed8629d1695760dabdfb9e21b1865f2
AlinaTertyshnikovaTR <alina.tertyshnikova@thomsonreuters.com>
2023-01-24 17:54:57 +0300

    implemented ssh executor

--- executor/executor.py
+++ executor/executor.py
@@ -1,14 +1,37 @@
 # Позже здесь будет функциональность для запуска тасок на удаленных машинах
+import abc
 import subprocess
 from typing import Tuple
 
+from paramiko.client import SSHClient
 
-class LocalExecutor:
+
+class Executor(abc.ABC):
     @staticmethod
-    def run_stage(stage_command: str) -> Tuple[str, str, int]:
+    @abc.abstractmethod
+    def run_stage(stage_command: str) -> Tuple[str, str]:
         """
         Execute a command
-        return stdout, stderr and exit code
+        return stdout, stderr
         """
-        command_result = subprocess.run(stage_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
-        return command_result.stdout, command_result.stderr, command_result.returncode
+        pass
+
+
+class LocalExecutor(Executor):
+    @staticmethod
+    def run_stage(stage_command: str) -> Tuple[str, str]:
+        command_result = subprocess.run(stage_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                                        text=True)
+        return command_result.stdout, command_result.stderr
+
+
+class SshExecutor(Executor):
+    @staticmethod
+    def run_stage(stage_command: str) -> Tuple[str, str]:
+        with SSHClient() as client:
+            client.load_system_host_keys()
+            client.connect(hostname="127.0.0.1", port=22, username="username")
+
+            _, stdout, stderr = client.exec_command(stage_command)
+            return stdout.read().decode("utf-8"), stderr.read().decode("utf-8")
+
--- runner/runner.py
+++ runner/runner.py
@@ -1,26 +1,27 @@
 from runner.task import TaskManager
-from executor.executor import LocalExecutor
+from executor.executor import LocalExecutor, SshExecutor
 from runner.status import Status
 
 
 class Runner:
     def __init__(self, task_manager: TaskManager):
         self._task_manager = task_manager
-        self._available_executors = [LocalExecutor()]
+        # self._available_executors = [LocalExecutor()]
+        self._available_executors = [SshExecutor()]
 
     def run(self):
         for task in self._task_manager:
             executor = self._available_executors.pop()
             for stage in task.stages:
                 stage.set_status(Status.RUNNING)
-                stdout, stderr, exit_code = executor.run_stage(stage.command)
-                if exit_code == 0:
-                    stage.set_status(Status.SUCCESS)
-                    stage.set_result(stdout)
-                else:
+                stdout, stderr = executor.run_stage(stage.command)
+                if stderr.strip():
                     stage.set_status(Status.FAILED)
                     stage.set_result(stderr)
                     break
+                else:
+                    stage.set_status(Status.SUCCESS)
+                    stage.set_result(stdout)
 
             task.set_done()
             self._task_manager.complete_task(task)
